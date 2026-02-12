e9471330c02b244b73d282471f16f282bfaa916c
Emmanuel-Bamidele <bamideleemmanuel02@gmail.com>
2026-02-11 16:20:04 -0800

    Initial Commit

---
+++ mini_redis/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mini_redis
+//
+//  Created by Emmanuel Bamidele on 2/11/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
